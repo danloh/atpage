7f81fe55d6c47ecc20da07553d4ffb31c741e58e
danloh <lploh2016@gmail.com>
2026-01-26 16:27:05 -0800

    mig leaflet to site.standard lexicons

--- src/resources/service/leaflet.rs
+++ src/resources/service/leaflet.rs
@@ -1,15 +1,14 @@
-//! leaflet pub 
-//! lexicons: 
+//! leaflet pub
+//! lexicons:
 //! https://tangled.org/@leaflet.pub/leaflet/blob/main/lexicons/pub/leaflet/document.json
+//! https://tangled.org/leaflet.pub/leaflet/blob/main/lexicons/site/standard/document.json
 
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 use crate::{
 	helper::utils::str_to_timestamp,
-	resources::{
-		atpage::AtRecord,
-		record::{get_record, list_record, uri_parts},
-	},
+	resources::{atpage::AtRecord, auth::resolve_did, record::{get_record, list_record, uri_parts}},
 };
 
 #[derive(Clone, Default, Serialize, Deserialize)]
@@ -31,9 +30,8 @@ pub struct LeafValue {
 	#[serde(rename = "$type")]
 	pub kind: String,
 	pub title: String,
-	pub author: String,
 	pub description: Option<String>,
-	pub publication: String, // at-uri: to get base_path
+	pub site: String, // at-uri: to get base_path
 	pub publishedAt: String,
 	// pub pages: Vec<LinerDocument>, // TODO
 }
@@ -50,18 +48,32 @@ pub async fn get_leaflet_records(
 				let cursor = data_res.cursor;
 				let entrylist: Vec<LeafResp> = data_res.records;
 				let mut recs: Vec<AtRecord> = Vec::new();
+				// as a cache to reduce the query base_path of publication(site)
+				let mut base_path_map: HashMap<String, String> = HashMap::new();
 				for entry in entrylist {
 					let value = entry.value;
-					let publication = value.publication;
-					let (p_did, _col, p_rkey) = uri_parts(&publication);
-					
-					let base_path = if let Some(val) = get_publication_record(&p_rkey, &p_did, serv).await {
-						val.base_path
-					} else {
-						continue;
-					};
 					let uri = entry.uri;
-					let (_did, _col, rkey) = uri_parts(&uri);
+					let (e_did, _col, rkey) = uri_parts(&uri);
+					let publication = value.site;
+
+					let base_path = if let Some(b_path) = base_path_map.get(&publication) {
+					  b_path.to_string()
+					} else {
+  			    let (p_did, _col, p_rkey) = uri_parts(&publication);
+  					let p_serv = if p_did == e_did {
+  					  serv
+  					} else {
+  			      &resolve_did(&p_did).await.service
+  					};
+					  if let Some(val) = get_publication_record(&p_rkey, &p_did, p_serv).await {
+  						let url = val.url;
+  						base_path_map.insert(publication, url.clone());
+  						url
+  					} else {
+  						continue;
+  					}
+					};
+
 					let link = if base_path.starts_with("https://") {
 						format!("{base_path}/{rkey}")
 					} else {
@@ -106,8 +118,8 @@ pub struct PublicationValue {
 	#[serde(rename = "$type")]
 	pub kind: String,
 	pub name: String,
+	pub url: String,
 	pub description: Option<String>,
-	pub base_path: String, 
 }
 
 async fn get_publication_record(id: &str, did: &str, serv: &str) -> Option<PublicationValue> {
@@ -115,7 +127,7 @@ async fn get_publication_record(id: &str, did: &str, serv: &str) -> Option<Publi
 		Ok(raw_data) => match serde_json::from_str::<PublicationResp>(&raw_data) {
 			Ok(data_res) => {
 				let data = data_res.value;
-				
+
 				return Some(data);
 			}
 			Err(_e) => {
